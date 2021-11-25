8a156f0c1d4a57bed1032df86bb034b078c9a718
Roman-Mordechai <roma2206@gmail.com>
2021-11-25 23:06:58 +0200

    ModelValidation

---
+++ MyWebApi/Controllers/ValuationController.cs
@@ -0,0 +1,22 @@
+﻿using MyWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MyWebApi.Controllers
+{
+    [RoutePrefix("vehicle")]
+    [ModelValidationActivationFilter]
+    public class ValuationController : ApiController
+    {
+        [HttpPost]
+        [Route("valuation")]
+        public IHttpActionResult GetValuation([FromBody] ValuationRequestModel model)
+        {
+            return Ok();
+        }
+    }
+}
